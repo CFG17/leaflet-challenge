b302f4ec53788e17b16e61a80e7b9e2102e599c7
Carmen Grayson <grayson17@gmail.com>
2021-01-21 21:20:44 -0500

    Add files via upload

---
+++ Leaflet challenge/Instructions/leaflet- step 2/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZ3JheXNvbjE3IiwiYSI6ImNram96Znk3cTAzeTUyeG53ZDEyNzd4cW8ifQ.r0sfjeq6Dib3WYZ69eKj_A";
---
+++ Leaflet challenge/Instructions/leaflet- step 2/static/js/logic.js
@@ -0,0 +1,140 @@
+// Create initial map object
+// Set the longitude, latitude, and the starting zoom level 
+// Inser into  div with an id of 'map' in index.html
+
+var myMap = L.map("map", {
+    center: [39.199093, -123.390632],
+    zoom: 13
+  });
+  
+  // Adding tile layer using addTo and 
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+
+  // Store API endpoint
+  var querylUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+  
+  // Get color radius to call query URL
+  d3.json(queryUrl, function(data) {
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+
+ // set different color from magnitude
+ function getColor(magnitude) {
+  switch (true) {
+  case magnitude > 5:
+    return "#ea2c2c";
+  case magnitude > 4:
+    return "#ea822c";
+  case magnitude > 3:
+    return "#ee9c00";
+  case magnitude > 2:
+    return "#eecc00";
+  case magnitude > 1:
+    return "#d4ee00";
+  default:
+    return "#98ee00";
+  }
+}
+// set radiuss from magnitude
+  function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+
+  return magnitude * 4;
+}
+  // GeoJSON layer
+  L.geoJson(data, {
+    // Maken cricles
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // cirecle style
+    style: styleInfo,
+    // popup for each marker
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(myMap);
+
+  // an object legend
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  // details for the legend
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // Looping through
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Finally, we our legend to the map.
+  legend.addTo(myMap);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
